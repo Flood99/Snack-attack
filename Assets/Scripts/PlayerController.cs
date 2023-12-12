0f3b3a03e0b153c36bc333319805c439d6542a7d
Flood99 <113184223+Flood99@users.noreply.github.com>
2023-12-12 13:14:39 -0500

    final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private int score = 0;
     public float speed = 5.0f;
+    public ParticleSystem particleSystemGood;
+    public ParticleSystem particleSystemBad;
+    private AudioSource audioSource;
+    public AudioClip goodSound;
+    public AudioClip badSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +23,23 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontal*speed*Time.deltaTime,0,vertical*speed*Time.deltaTime));
+        audioSource = GetComponent<AudioSource>();
        
     }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Good"))
         {
+            audioSource.clip = goodSound;
+            audioSource.Play();
+            particleSystemGood.Play();
             score += 1;
             Debug.Log(score);
             Destroy(other.gameObject);
         }else if(other.gameObject.CompareTag("Bad")){
+            audioSource.clip = badSound;
+            audioSource.Play();
+             particleSystemBad.Play();
             score -= 1;
             Debug.Log(score);
             Destroy(other.gameObject);
